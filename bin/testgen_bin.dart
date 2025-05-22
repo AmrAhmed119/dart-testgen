0e71968cf50ccadea8ac7598db51c2950f40c899
AmrAhmed119 <es-AmrA.Abdelazim2025@alexu.edu.eg>
2025-05-22 07:29:48 +0300

    Initial project setup of package-template

---
+++ bin/testgen_bin.dart
@@ -0,0 +1,3 @@
+void main(List<String> args) {
+  // parse the command line arguments
+}
---
+++ example/testgen_example.dart
@@ -0,0 +1,6 @@
+import 'package:testgen/testgen.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
