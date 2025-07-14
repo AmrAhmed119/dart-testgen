4166950976cc958354c3fc94da0e110c1e8ed125
AmrAhmed119 <es-AmrA.Abdelazim2025@alexu.edu.eg>
2025-07-14 05:49:56 +0300

    Add relevant comments explaining visitor logic

--- lib/src/analyzer/visitor.dart
+++ lib/src/analyzer/visitor.dart
@@ -29,19 +29,34 @@ class DependencyVisitor extends RecursiveAstVisitor<void> {
     }
   }
 
+  // Captures type references such as class names, type parameters.
   @override
   void visitNamedType(ast.NamedType node) {
     _addDependencyById(node.element2?.id);
     super.visitNamedType(node);
   }
 
+  // Captures variable references, property access, and method calls.
+  // Examples:
+  // - Variable references: myVariable, globalVar
+  // - Property access: obj.property (captures 'property')
+  // - Method calls: obj.method() (captures 'method')
+  // - Getter access: obj.value (captures underlying variable if it's a property)
+  // - TODO: Setter access: currently has a problem with setters.
+  //
+  // This method is sufficient for most dependencies since SimpleIdentifier
+  // tokens are used throughout the Dart grammar and will be  visited
+  // automatically by the recursive visitor for most language constructs.
   @override
   void visitSimpleIdentifier(ast.SimpleIdentifier node) {
     final element = node.element;
+    _addDependencyById(element?.id);
+
+    // Handle the case where the element is an implicit getter so we need to
+    // capture the underlying variable id.
     if (element is PropertyAccessorElement2) {
       _addDependencyById(element.variable3?.id);
     }
-    _addDependencyById(element?.id);
     super.visitSimpleIdentifier(node);
   }
 }
@@ -62,10 +77,11 @@ class VariableDependencyVisitor extends DependencyVisitor {
   //   the variable(s).
   //
   // - Visit the specific VariableDeclaration to capture any type information
-  //   defined directly for that variable since we call the accept on each
-  //   variable defined in the variable list.
+  //   defined directly for that variable since we call the accept method on
+  //   each variable defined in the variable list.
   //
-  // The types are then captured from the visitNamedType function.
+  // This is necessary to avoid capturing dependencies from other variables in
+  // the same variable list
   @override
   void visitVariableDeclarationList(ast.VariableDeclarationList node) {
     node.type?.accept(this);
@@ -94,6 +110,12 @@ class CompoundDependencyVisitor extends DependencyVisitor {
       interface.accept(this);
     }
   }
+
+  // For all overridden methods below, we do not call super because we do not
+  // need to recursively visit all compound declaration members (such as fields,
+  // methods, or constructors). Instead, we only extract dependencies from the
+  // relevant clauses (extends, implements, mixins, constraints, etc.) that define
+  // type relationships for the declaration.
 
   @override
   void visitClassDeclaration(ast.ClassDeclaration node) {
