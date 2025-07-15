21ddb0d5380092219a20663df90ccfb868e4178c
AmrAhmed119 <es-AmrA.Abdelazim2025@alexu.edu.eg>
2025-07-16 00:49:54 +0300

    Test class members dependencies

---
+++ test/analyzer/dependency_graph/code/class_members.dart
@@ -0,0 +1,59 @@
+int globalVar1 = 30;
+
+void globalFunc(int x) {}
+
+mixin Logger {
+  void log(String msg) {}
+}
+
+class Class1 {
+  String field1 = 'test';
+
+  void method1() {}
+}
+
+extension StringExtension on String {
+  void method2() {}
+}
+
+enum Enum {
+  value1(0),
+  value2(1),
+  value3(3);
+
+  final int code;
+
+  const Enum(this.code);
+}
+
+class Class2 extends Class1 with Logger {
+  String field2 = 'name';
+  int _field3 = 1;
+  Class2? _field4;
+  int conditionalVar = globalVar1 == 1 ? Enum.value1.code : 3;
+
+  Class2.named(this.field2, Class2 c2) {
+    _field4 = c2;
+    print(_field3);
+    method3();
+  }
+
+  void method3() {}
+
+  Class2? get field4 => _field4;
+
+  set field3(int i) => _field3 = i;
+
+  void method4() {
+    print(globalVar1);
+    globalFunc(1);
+    log('test');
+    print(field1);
+    method1();
+    'test'.method2();
+    print(Enum.value1);
+    print(field2);
+    print(field4);
+    field3 = 3;
+  }
+}
---
+++ test/analyzer/dependency_graph/dependency_test/class_members_test.dart
@@ -0,0 +1,98 @@
+import 'package:test/test.dart';
+import 'package:testgen/src/analyzer/declaration.dart';
+import 'package:path/path.dart' as path;
+
+import '../../utils.dart';
+
+void main() {
+  late Map<String, Declaration> decls;
+
+  setUpAll(() async {
+    decls = await extractDeclarationsForSourceFile(
+      path.join(
+        'test',
+        'analyzer',
+        'dependency_graph',
+        'code',
+        'class_members.dart',
+      ),
+      [
+        'globalVar1',
+        'globalFunc',
+        'Logger',
+        'log',
+        'Class1',
+        'field1',
+        'method1',
+        'StringExtension',
+        'method2',
+        'Enum',
+        'value1',
+        'code',
+        'Class2',
+        'field2',
+        '_field3',
+        '_field4',
+        'conditionalVar',
+        'Class2.named',
+        'method3',
+        'field4',
+        'field3',
+        'method4',
+      ],
+    );
+  });
+
+  group('Test Class Members Declarations Dependencies', () {
+    test('Test Constructor Dependencies', () {
+      expect(decls['Class2.named']!.dependsOn, hasLength(3));
+      expect(
+        decls['Class2.named']!.dependsOn,
+        containsAll([decls['Class2'], decls['_field3'], decls['method3']]),
+      );
+    });
+
+    test('Test Method Dependencies', () {
+      expect(decls['method4']!.dependsOn, hasLength(10));
+      expect(
+        decls['method4']!.dependsOn,
+        containsAll([
+          decls['globalVar1'],
+          decls['globalFunc'],
+          decls['log'],
+          decls['field1'],
+          decls['method1'],
+          decls['method2'],
+          decls['Enum'],
+          decls['value1'],
+          decls['field2'],
+          decls['field4'],
+        ]),
+      );
+    });
+
+    test('Test Field Dependencies', () {
+      expect(decls['field1']!.dependsOn, isEmpty);
+
+      expect(decls['_field4']!.dependsOn, hasLength(1));
+      expect(decls['_field4']!.dependsOn, contains(decls['Class2']));
+
+      expect(decls['field4']!.dependsOn, hasLength(2));
+      expect(
+        decls['field4']!.dependsOn,
+        containsAll([decls['_field4'], decls['Class2']]),
+      );
+
+      expect(decls['conditionalVar']!.dependsOn, hasLength(4));
+      expect(
+        decls['conditionalVar']!.dependsOn,
+        containsAll([
+          decls['globalVar1'],
+          decls['Enum'],
+          decls['value1'],
+          decls['code'],
+        ]),
+      );
+    });
+  });
+}
